6610e2a40d1afb5614451df935223376e135ece2
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-23 08:27:08 +0200

    Buildable all

--- _sunamo/SunamoInterfaces/Interfaces/IClipboardHelper.cs
+++ _sunamo/SunamoInterfaces/Interfaces/IClipboardHelper.cs
@@ -4,6 +4,6 @@ namespace SunamoClipboard;
 /// <summary>
 ///     Must be in sunamo, is used in win and apps
 /// </summary>
-internal interface IClipboardHelper : IClipboardHelperBase<string, List<string>, bool>
+public interface IClipboardHelper : IClipboardHelperBase<string, List<string>, bool>
 {
 }
