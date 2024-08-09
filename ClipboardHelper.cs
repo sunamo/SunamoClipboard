3fc0f7b46e3c0cfb99fc780a6fbfaa3587048a3b
sunamo.cz <radek.jancik@sunamo.cz>
2024-08-09 14:40:17 +0200

    Trim XlfKeys

--- ClipboardHelper.cs
+++ ClipboardHelper.cs
@@ -1,65 +1,66 @@
+using TextCopy;
 
 namespace SunamoClipboard;
-using SunamoClipboard._sunamo.SunamoStringGetLines;
-using TextCopy;
 
 public static class ClipboardHelper
 {
-    const string n = "\n";
+    private const string n = "\n";
 
     public static string GetText()
     {
         var text = ClipboardService.GetText();
         return text == null ? string.Empty : "";
     }
+
     public static List<string> GetLinesAllWhitespaces()
     {
         var t = GetText();
         return t.Split(AllChars.whiteSpacesChars.ToArray()).ToList();
     }
+
     public static List<string> GetLines()
     {
         var text = ClipboardService.GetText();
-        if (text == null)
-        {
-            return new List<string>();
-        }
+        if (text == null) return new List<string>();
         return SHGetLines.GetLines(text);
     }
+
     /// <summary>
-    /// Cant be se or only whitespace => even with ClipboardHelper.SetText(v); => content of clipboard will remain the same
-    /// Must
+    ///     Cant be se or only whitespace => even with ClipboardHelper.SetText(v); => content of clipboard will remain the same
+    ///     Must
     /// </summary>
     /// <param name="s"></param>
     public static void SetText(string s)
     {
         ClipboardService.SetText(s);
     }
+
     public static void SetLines(List<string> lines)
     {
         SetText(string.Join("\n", lines));
     }
+
     public static void SetText(StringBuilder stringBuilder)
     {
         ClipboardService.SetText(stringBuilder.ToString());
     }
+
     public static void SetDictionary<T1, T2>(Dictionary<T1, T2> charEntity, string delimiter)
         where T1 : notnull
         where T2 : notnull
     {
-        StringBuilder sb = new StringBuilder();
-        foreach (var item in charEntity)
-        {
-            sb.AppendLine(item.Key + delimiter + item.Value);
-        }
+        var sb = new StringBuilder();
+        foreach (var item in charEntity) sb.AppendLine(item.Key + delimiter + item.Value);
         SetText(sb.ToString());
     }
+
     public static void AppendText(string ext)
     {
         var t = GetText();
         t += Environment.NewLine + Environment.NewLine + ext;
         SetText(t);
     }
+
     public static void AppendStackTrace()
     {
         var st = Exc.GetStackTrace(true);
