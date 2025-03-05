ad785c450dc347db936f3e8c159585a520add747
sunamo.cz <radek.jancik@sunamo.cz>
2025-03-05 07:29:11 +0100

    Added global using of Microsoft.Extensions.Logging.Abstractions to all projects

--- ClipboardHelper.cs
+++ ClipboardHelper.cs
@@ -1,33 +1,25 @@
 namespace SunamoClipboard;
 
-
-using TextCopy;
-
 public static class ClipboardHelper
 {
     private const string n = "\n";
-
     public static string GetText()
     {
         var text = ClipboardService.GetText();
         return text == null ? string.Empty : "";
     }
-
     public static List<string> GetLinesAllWhitespaces()
     {
         WhitespaceCharService whitespaceChars = new WhitespaceCharService();
-
         var t = GetText();
         return t.Split(whitespaceChars.whiteSpaceChars.ToArray()).ToList();
     }
-
     public static List<string> GetLines()
     {
         var text = ClipboardService.GetText();
         if (text == null) return new List<string>();
         return SHGetLines.GetLines(text);
     }
-
     /// <summary>
     ///     Cant be se or only whitespace => even with ClipboardHelper.SetText(v); => content of clipboard will remain the same
     ///     Must
@@ -37,17 +29,14 @@ public static class ClipboardHelper
     {
         ClipboardService.SetText(s);
     }
-
     public static void SetLines(List<string> lines)
     {
         SetText(string.Join("\n", lines));
     }
-
     public static void SetText(StringBuilder stringBuilder)
     {
         ClipboardService.SetText(stringBuilder.ToString());
     }
-
     public static void SetDictionary<T1, T2>(Dictionary<T1, T2> charEntity, string delimiter)
         where T1 : notnull
         where T2 : notnull
@@ -56,12 +45,10 @@ public static class ClipboardHelper
         foreach (var item in charEntity) sb.AppendLine(item.Key + delimiter + item.Value);
         SetText(sb.ToString());
     }
-
     public static void AppendText(string ext)
     {
         var t = GetText();
         t += Environment.NewLine + Environment.NewLine + ext;
         SetText(t);
     }
-
 }
