4d1fc0cc466a70db04ff5ef88cf9bb382dd599de
smutekutek <smutekutek@gmail.com>
2026-06-24 22:25:46 +0200

    Ahoj! Jak ti mohu pomoci?

--- SunamoClipboard/ClipboardHelper.cs
+++ SunamoClipboard/ClipboardHelper.cs
@@ -1,77 +1,42 @@
 namespace SunamoClipboard;
 
-/// <summary>
-/// Provides helper methods for working with the system clipboard.
-/// </summary>
 public static class ClipboardHelper
 {
-    /// <summary>
-    /// Gets text from the clipboard.
-    /// </summary>
-    /// <returns>The text from the clipboard, or an empty string if the clipboard is empty or null.</returns>
     public static string GetText()
     {
         var text = ClipboardService.GetText();
         return text ?? string.Empty;
     }
 
-    /// <summary>
-    /// Gets lines from the clipboard by splitting on all whitespace characters.
-    /// </summary>
-    /// <returns>A list of strings split by whitespace characters.</returns>
     public static List<string> GetLinesAllWhitespaces()
     {
-        WhitespaceCharService whitespaceCharService = new WhitespaceCharService();
+        WhitespaceCharService whitespaceCharService = new();
         var text = GetText();
         return text.Split(whitespaceCharService.WhiteSpaceChars.ToArray()).ToList();
     }
 
-    /// <summary>
-    /// Gets lines from the clipboard by splitting on newline characters.
-    /// </summary>
-    /// <returns>A list of lines from the clipboard text.</returns>
     public static List<string> GetLines()
     {
         var text = ClipboardService.GetText();
-        if (text == null) return new List<string>();
+        if (text is null) return [];
         return SHGetLines.GetLines(text);
     }
 
-    /// <summary>
-    /// Sets text to the clipboard.
-    /// Cannot be empty or only whitespace - if the text is empty, the clipboard content will remain unchanged.
-    /// </summary>
-    /// <param name="text">The text to set to the clipboard.</param>
     public static void SetText(string text)
     {
         ClipboardService.SetText(text);
     }
 
-    /// <summary>
-    /// Sets multiple lines to the clipboard, joined by newline characters.
-    /// </summary>
-    /// <param name="list">The list of lines to set to the clipboard.</param>
     public static void SetLines(List<string> list)
     {
         SetText(string.Join("\n", list));
     }
 
-    /// <summary>
-    /// Sets text from a StringBuilder to the clipboard.
-    /// </summary>
-    /// <param name="stringBuilder">The StringBuilder containing the text to set to the clipboard.</param>
     public static void SetText(StringBuilder stringBuilder)
     {
         ClipboardService.SetText(stringBuilder.ToString());
     }
 
-    /// <summary>
-    /// Sets a dictionary to the clipboard, with each key-value pair on a separate line.
-    /// </summary>
-    /// <typeparam name="T1">The type of the dictionary keys.</typeparam>
-    /// <typeparam name="T2">The type of the dictionary values.</typeparam>
-    /// <param name="dictionary">The dictionary to set to the clipboard.</param>
-    /// <param name="delimiter">The delimiter to use between keys and values.</param>
     public static void SetDictionary<T1, T2>(Dictionary<T1, T2> dictionary, string delimiter)
         where T1 : notnull
         where T2 : notnull
@@ -81,10 +46,6 @@ public static class ClipboardHelper
         SetText(stringBuilder.ToString());
     }
 
-    /// <summary>
-    /// Appends text to the existing clipboard content with two newlines as separator.
-    /// </summary>
-    /// <param name="textToAppend">The text to append to the clipboard.</param>
     public static void AppendText(string textToAppend)
     {
         var currentText = GetText();
